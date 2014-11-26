729da678a8681d086e1f01f809a642afd25ca095
Chhaya <chhaya11@163.com>
2014-11-26 08:34:17 +0800

    文本处理

---
+++ cp2_strings_and_text/splitting_string.py
@@ -0,0 +1,302 @@
+# coding:utf8
+
+### 切分字符串
+import re
+line = 'asf asdf; xxx, asdf,sfd, aaa,   i; jjj'
+# 可自定义分割字符
+print(re.split(r'[\s;,]\s*', line))
+# ['asf', 'asdf', 'xxx', 'asdf', 'sfd', 'aaa', 'i', 'jjj']
+
+# 要注意括号为捕获组
+print(re.split(r'(\s|;|,)\s*', line))
+# ['asf', ' ', 'asdf', ';', 'xxx', ',', 'asdf', ',', 'sfd', ',', 'aaa', ',', 'i', ';', 'jjj']
+
+# 加?:变成非捕获组
+print(re.split(r'(?:\s|;|,)\s*', line))
+
+### startswith和endswith多个选择
+# 一个的情况：
+filename = "spam.txt"
+_ = filename.endswith(".txt")
+print(_)
+# 多个的情况：
+import os
+filenames = os.listdir('/Users/alpha/Downloads/setups/wget-1.16/src')
+# 选出所有.c和.h的文件
+print([name for name in filenames if name.endswith(('.c', '.h'))])
+# 是否有文件是.c
+print(any(name.endswith('.c') for name in filenames))
+# 必须要是tuple
+line.startswith(tuple(['.c', '.h']))
+
+
+### 使用shell通配符匹配字符串
+from fnmatch import fnmatch, fnmatchcase
+print(fnmatch('foo.txt', '*.txt'))
+print(fnmatch('foo.txt', '?oo.txt'))
+print(fnmatch('Dat45.csv', 'Dat[0-9]*'))
+print(fnmatchcase('foo.txt', '*.TXT'))  # 区分大小写
+
+### 匹配和搜索
+# 简单的使用 ==, str.find(), str.endswith(), str.startswith()
+# 复杂的用re
+text1 = '11/24/2012'
+text2 = 'Nov 23, 2012'
+if re.match(r'\d+/\d+/\d+', text1):
+    print('yes')
+else:
+    print('no')
+
+# 预编译方法
+datepat = re.compile(r'\d+/\d+/\d+')
+print(datepat.match(text1))  # obj
+print(datepat.match(text2))  # None
+
+# 搜索所有的
+text = 'today is 11/24/2012, tomorrow is 12/24/2012'
+print(datepat.findall(text))  # [item1, item2]
+
+# 一个的时候捕获组
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+m = datepat.match(text1)
+print(m.group(0))  # 11/24/2012
+print(m.group(1))  # 11
+print(m.groups())  # ('11', '24', '2012')
+
+# 多个的时候捕获组
+print(datepat.findall(text))  # [('11', '24', '2012'), ('12', '24', '2012')]
+for month, day, year in datepat.findall(text):
+    print('{}-{}-{}'.format(month, day, year))
+
+# match 是从头开始搜的，要匹配整个可以用
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)$')
+
+
+### 搜索和替换
+# 简单的可用str.replace()
+# 复杂的用re.sub()
+print(text)
+print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', text))
+# today is 11/24/2012, tomorrow is 12/24/2012
+# today is 2012-11-24, tomorrow is 2012-12-24
+
+# 使用预编译
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+print(datepat.sub(r'\3-\1-\2', text))
+
+# 使用callback处理更复杂情况
+from calendar import month_abbr
+def change_date(m):
+    mon_name = month_abbr[int(m.group(1))]  # 数字的月转为字母的月
+    return '{} {} {}'.format(m.group(3), mon_name, m.group(2))
+print(datepat.sub(change_date, text))
+# today is 2012 Nov 24, tomorrow is 2012 Dec 24
+
+# 获得替换个数
+newtext, n = datepat.subn(change_date, text)
+print(newtext)
+print(n)
+
+
+### 忽略大小写搜索
+#re.findall(datepat, text, flags=re.IGNORECASE)
+#re.sub(datepat, text, flags=re.IGNORECASE)
+
+# 如果替换的时候要保持原来的大小写
+text = 'UPPER PYTHON, lowser python, Mixed Python'
+def matchcase(word):
+    def replace(m):
+        text = m.group()
+        print('{}-{}'.format(word, text))
+        if text.islower():
+            return word.lower()
+        elif text.isupper():
+            return word.upper()
+        elif text[0].isupper():
+            return word.capitalize()
+        else:
+            return word
+    return replace
+print(re.sub('python', matchcase('snake'), text, flags=re.IGNORECASE))
+
+### 最短匹配, 在+或*后面加?
+text1 = '"yes" or "no".'
+str_pat = re.compile(r'\"(.*)\"')
+print(str_pat.findall(text1))
+# ['yes" or "no']
+str_pat = re.compile(r'\"(.*?)\"')
+print(str_pat.findall(text1))
+# ['yes', 'no']
+
+### 多行匹配
+# .不匹配换行
+# 法1，正则里加\n支持 (推荐)
+comment = re.compile(r'/\*((?:.|\n)*?)\*/')
+text2 = """/* this is a
+    multiline comment*/"""
+print(comment.findall(text2))
+
+# 法2，用DOTALL flag
+comment = re.compile(r'/\*(.*?)\*/', re.DOTALL)
+print(comment.findall(text2))
+
+
+# 标准化unicode文本
+# 相同的字符串用不同的代码表达
+s1 = 'Spicy Jalape\u00f1o'
+s2 = 'Spicy Jalapen\u0303o'
+print(s1)
+print(s2)
+print(s1 == s2)
+# Spicy Jalapeño
+# Spicy Jalapeño
+# False
+import unicodedata
+# NFC 表示字符尽量用简单code构成
+t1 = unicodedata.normalize('NFC', s1)
+t2 = unicodedata.normalize('NFC', s2)
+print(ascii(t1))
+# 'Spicy Jalape\xf1o'
+print(t1 == t2)
+# True
+
+# NFD 表示字符尽量用组合code构成
+t1 = unicodedata.normalize('NFD', s1)
+t2 = unicodedata.normalize('NFD', s2)
+print(ascii(t1))
+# 'Spicy Jalapen\u0303o'
+print(t1 == t2)
+# True
+
+# 用来过滤掉可区别的mark
+# combining() 用来测试是否是个结合字符
+t1 = unicodedata.normalize('NFD', s1)
+print(''.join(c for c in t1 if not unicodedata.combining(c)))
+# Spicy Jalapeno
+
+
+### 正则匹配处理unicode字符 （推荐regex库）
+# 匹配阿拉伯字符
+arabic = re.compile('[\u0600-\u06ff\u0750-\u077f\u08a0\u08ff]')
+
+# 匹配前最好先让所有的文本都标准化
+# 也要注意特殊例子
+pat = re.compile('stra\u00dfe', re.IGNORECASE)
+s = 'straße'
+print('stra\u00dfe')
+print(pat.match(s))  # match
+print(pat.match(s.upper()))  # None
+print(s.upper())  # STRASSE
+
+### 去掉不想要的字符
+# 用str.strip(), lstrip(), rstrip()
+# 文件中读出来去掉前后空白
+# with open(filename) as fp:
+#     lines = (line.strip() for line in fp)  # efficient！
+#     for line in lines:
+#         ....
+# 更高级用translate
+
+
+### 审查和clean up 文本
+s = 'python\fis\tawesome\r\n'
+# 先去掉空白
+remap = {
+    ord('\t'): ' ',
+    ord('\f'): ' ',
+    ord('\r'): None  # delete
+}
+a = s.translate(remap)
+print(a)
+# python is awesome
+# 再去掉其他组合字符
+# import sys
+# cmb_chrs = dict.fromkeys(c for c in range(sys.maxunicode) if unicodedata.combining(chr(c)))
+# b = unicodedata.normalize('NFD', a)
+# b.translate(cmb_chrs)
+# 通过fromkeys，把那些音标都过滤为None， 就去掉了那些音标
+
+# 把所有的unicode十进制数字转为对应的ASCII
+# digitmap = {c: ord('0') + unicodedata.digit(chr(c))
+#             for c in range(sys.maxunicode)
+#             if unicodedata.category(chr(c)) == 'Nd'}
+# print(len(digitmap))  # 460
+# x = '\u0661\u0662\u0663'
+# print(x.translate(digitmap))
+# 123
+
+# 从性能上来说str.replace()是最快的。
+
+
+### 校准文本字符串
+# 简单的用ljust, rjust, center
+text = 'Hello world'
+print(text.ljust(20))
+print(text.rjust(20))
+print(text.center(20))
+# Hello world
+#          Hello world
+#     Hello world
+
+# 可指定填充字符
+print(text.rjust(20, '*'))
+# *********Hello world
+
+# format
+print(format(text, '>20'))
+print(format(text, '<20'))
+print(format(text, '^20'))
+# Hello world
+#          Hello world
+#     Hello world
+
+# 带填充的format
+print(format(text, '=>20s'))
+# =========Hello world
+
+# format处理多个值
+print('{:>10s} {:>10s}'.format('Hello', 'world'))
+#      Hello      world
+
+# 对于数字
+x = 1234.5
+print(format(x, '>10'))
+print(format(x, '^10.2f'))
+#     1234.5
+#  1234.50
+
+# % 是老方法了， 推荐用format
+
+
+### 连接字符串
+# 一般用str.join(list)
+# 用+连接很多字符串是很耗性能的，因为每次 += 操作都会生成一个新的字符串对象，要用join来替换
+a = 'a'
+b = 'b'
+c = 'c'
+print(a + ':' + b + ':' + c)  # 丑
+print(':'.join([a, b, c]))  # 丑
+print(a, b, c, sep=':')  # 好
+
+# 下面的例子是优雅得将一大串字符串写到文件中
+# generator object
+def sample():
+    yield 'Is'
+    yield 'Chicage'
+    yield 'Not'
+    yield 'Chicage?'
+
+def combine(source, maxsize):
+    parts = []
+    size = 0
+    for part in source:
+        parts.append(part)
+        size += len(part)
+        if size > maxsize:
+            yield ''.join(parts)
+            parts = []
+            size = 0
+    yield ''.join(parts)
+f = open('temp.txt', 'w')
+for part in combine(sample(), 32767):
+    f.write(part)
