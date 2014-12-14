e5204af9371493568fdcf65a65d54e3d5e83eeac
Chhaya <chhaya11@163.com>
2014-12-14 16:59:55 +0800

    读写csv 一些改进

--- ch6/csvtest.py
+++ ch6/csvtest.py
@@ -1,6 +1,9 @@
 import csv
 
 # 写CSV (tuple)
+# 默认写进去是逗号分隔的，用number打开正常，用excel打开不正常。
+# writer时指定delimiter为分号再写，excel打开就正常了
+# 同时reader的时候也要指定delimiter为分号
 headers = ['Symbol', 'Price', 'Date', 'Time', 'Change', 'Volume']
 rows = [('AA', 39.48, '6/11/2011', '9:36am', -0.18, 181800),
         ('AIG', 39.48, '6/11/2011', '9:36am', -0.18, 181800),
@@ -8,18 +11,18 @@ rows = [('AA', 39.48, '6/11/2011', '9:36am', -0.18, 181800),
         ('ABC', 39.48, '6/11/2011', '9:36am', -0.18, 181800),
         ('ES', 39.48, '6/11/2011', '9:36am', -0.18, 181800)
         ]
-# with open('stock.csv', 'w') as f:
-#     print(dir(csv))
-#     f_csv = csv.writer(f)
-#     f_csv.writerow(headers)
-#     f_csv.writerows(rows)
+with open('stock.csv', 'w') as f:
+    print(dir(csv))
+    f_csv = csv.writer(f, delimiter=';')
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
 
 # 写CSV (dict)
-rows2 = [{'Symbol': 'AA', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':'181800'},
-{'Symbol': 'AG', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':'181800'},
-{'Symbol': 'AX', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':'181800'},
-{'Symbol': 'AC', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':'181800'},
-{'Symbol': 'ES', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':'181800'}]
+rows2 = [{'Symbol': 'AA', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':81800},
+{'Symbol': 'A G', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':181800},
+{'Symbol': 'AX', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':181800},
+{'Symbol': 'AC', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':181800},
+{'Symbol': 'ES', 'Price':39.48, 'Date':'6/11/2011', 'Time':'9:36am', "Change":-0.18, 'Volume':181800}]
 
 with open('stock2.csv', 'w') as f:
     f_csv = csv.DictWriter(f, headers)
@@ -27,11 +30,16 @@ with open('stock2.csv', 'w') as f:
     f_csv.writerows(rows2)
 
 # 读CSV
-with open('stock.csv') as f:
-    f_csv = csv.reader(f)
-    headers = next(f_csv)
+import re
+col_types = [str, float, str, str, float, int]
+with open('stock2.csv') as f:
+    f_csv = csv.reader(f, delimiter=',')
+    # 如果header中带特殊字符的话，需要转成合法的才能读
+    headers = [re.sub('[^a-zA-Z_]', '_', h) for h in next(f_csv)]
     print(headers)
     for row in f_csv:
+        # 默认都是字符串， 需要自己转换类型
+        row = tuple(convert(value) for convert, value in zip(col_types, row))
         print(row)
 
 # 用namedtuple读
